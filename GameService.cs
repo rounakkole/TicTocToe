55416cbd28ecedd8d336ea8aa865ef63a4a606a2
rounak <rounakkole@gmail.com>
2022-05-07 22:15:18 +0530

    Add project files.

---
+++ GameService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTocToe
+{
+    internal class GameService
+    {
+
+        public int GetPosition(int[] RepeatArray)
+        {
+            Random random = new Random();
+
+            bool IsRepeat;
+            int Position;
+            do
+            {
+                IsRepeat = false;
+                Position = random.Next(0, 9);
+                if (RepeatArray[Position] > 0)
+                {
+                    IsRepeat = true;
+                }
+
+
+            } while (IsRepeat == true);
+            return Position;
+        }
+
+        public bool GameOver(int[] RepeatArray)
+        {
+            bool IsOver = false;
+            bool IsEmpty = false;
+            foreach (int num in RepeatArray)
+            {
+                if (num < 1)
+                {
+                    IsEmpty = true;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (RepeatArray[i] == RepeatArray[i + 3] && RepeatArray[i] == RepeatArray[i + 6] && RepeatArray[i] != 0)
+                {
+                    IsOver = true;
+                }
+
+                if (RepeatArray[i * 3] == RepeatArray[i * 3 + 1] && RepeatArray[i * 3] == RepeatArray[i * 3 + 2] && RepeatArray[i * 3] != 0)
+                {
+                    IsOver = true;
+                }
+            }
+
+
+            if (RepeatArray[0] == RepeatArray[4] && RepeatArray[0] == RepeatArray[8] && RepeatArray[0] != 0)
+            {
+                IsOver = true;
+            }
+            else if (RepeatArray[2] == RepeatArray[4] && RepeatArray[2] == RepeatArray[6] && RepeatArray[2] != 0)
+            {
+                IsOver = true;
+            }
+
+
+            if (IsEmpty == false)
+            {
+                IsOver = true;
+            }
+            return IsOver;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace TicTocToe
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t Tic Toc Toe");
+            GamePlay();
+        }
+
+
+        public static void GamePlay()
+        {
+            int[] RepeatArray = new int[9];
+            bool IsOver = false;
+            bool PlayTurn = true;
+            GameService gameService = new GameService();
+
+
+            while (IsOver == false)
+            {
+                int Position = gameService.GetPosition(RepeatArray);
+
+                if (PlayTurn)
+                {
+                    RepeatArray[Position] = 1;
+                    PlayTurn = false;
+                }
+                else
+                {
+                    RepeatArray[Position] = 2;
+                    PlayTurn = true;
+                }
+
+                IsOver = gameService.GameOver(RepeatArray);
+            }
+
+
+            for (int i = 0; i < 9; i++)
+            {
+                Console.Write("\t{0}", RepeatArray[i]);
+                if ((i + 3) % 3 == 2)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
